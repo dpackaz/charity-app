c1ae2a697d6cb7d8d817a37bdafb3b4d0dbd6c77
PeterM012 <110750833+PeterM012@users.noreply.github.com>
2022-12-10 12:53:09 -0800

    add new navbar component and updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from "./components/Navbar";
 import Home from './pages/Home';
 
 function App() {
   return (
     <Router>
       <div className="flex-column justify-center align-center min-100-vh bg-primary">
+      <Navbar />
         <Routes>
           <Route 
             path="/" 
