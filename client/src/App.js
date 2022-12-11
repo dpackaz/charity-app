30bfcb6f1dd6aeda0e8de9a05a9160b2490c0bf8
Philip Huang <philiphuang2@gmail.com>
2022-12-11 12:42:30 -0800

    working map done

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-
+import Landing from './pages/Landing';
 function App() {
   return (
     <>
@@ -11,6 +11,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path='/testing' element= {<Landing />} />
         </Routes>
       </div>
     </Router>
