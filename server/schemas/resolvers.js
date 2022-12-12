6777dfb0bb80ee9f17063714c1e17a4b2ae7e828
Daniel Packer <danielapacker@gmail.com>
2022-12-12 12:46:45 -0700

    update formatting in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,49 +1,41 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { signToken } = require('../utils/auth');
-
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
 //TODO: create resolvers
-const resolvers={
-    Query: {
-        users: async () => {
-            return await User.find({});
-          },
-    }
-    Query: {
-        charities: async () => {
-            return await Charity.find({});
-        }
-    }
-    Query: {
-        charity: async (args) => {
-            return await Charity.findById(args.id);
-        }
-    }
-    Query: {
-        drive: async () => {
-            return await Drive.find({}).populate('users').populate({
-              path: 'users',
-              populate: 'charity'
-            });
-          },
-    }
-    Mutation: {
-        addUser: async ({ name, email, password }) => {
-          return await User.create({ name, email, password });
-        },
-        addDrive: async ({ user, charity, goal }) => {
-          return await Drive.create({ user, charity, goal });
-        },
-        updateDrive: async (parent, { id, charity, goal }) => {
-            return await Drive.findOneAndUpdate(
-                { _id: id },
-                { charity },
-                { goal },
-                { new: true }
-            );
-        }
-    }
-
+const resolvers = {
+  Query: {
+    users: async () => {
+      return await User.find({});
+    },
+    charities: async () => {
+      return await Charity.find({});
+    },
+    charity: async (args) => {
+      return await Charity.findById(args.id);
+    },
+    drive: async () => {
+      return await Drive.find({}).populate("users").populate({
+        path: "users",
+        populate: "charity",
+      });
+    },
+  },
+  Mutation: {
+    addUser: async ({ name, email, password }) => {
+      return await User.create({ name, email, password });
+    },
+    addDrive: async ({ user, charity, goal }) => {
+      return await Drive.create({ user, charity, goal });
+    },
+    updateDrive: async (parent, { id, charity, goal }) => {
+      return await Drive.findOneAndUpdate(
+        { _id: id },
+        { charity },
+        { goal },
+        { new: true }
+      );
+    },
+  },
 };
 
 module.exports = resolvers;
