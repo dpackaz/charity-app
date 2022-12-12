4f2882d7d1383d86b89845d782ca67458f0703f4
Daniel Packer <danielapacker@gmail.com>
2022-12-12 12:43:46 -0700

    add typeDefs and work on resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,8 +5,45 @@ const { signToken } = require('../utils/auth');
 //TODO: create resolvers
 const resolvers={
     Query: {
-
+        users: async () => {
+            return await User.find({});
+          },
     }
+    Query: {
+        charities: async () => {
+            return await Charity.find({});
+        }
+    }
+    Query: {
+        charity: async (args) => {
+            return await Charity.findById(args.id);
+        }
+    }
+    Query: {
+        drive: async () => {
+            return await Drive.find({}).populate('users').populate({
+              path: 'users',
+              populate: 'charity'
+            });
+          },
+    }
+    Mutation: {
+        addUser: async ({ name, email, password }) => {
+          return await User.create({ name, email, password });
+        },
+        addDrive: async ({ user, charity, goal }) => {
+          return await Drive.create({ user, charity, goal });
+        },
+        updateDrive: async (parent, { id, charity, goal }) => {
+            return await Drive.findOneAndUpdate(
+                { _id: id },
+                { charity },
+                { goal },
+                { new: true }
+            );
+        }
+    }
+
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,13 +2,27 @@ const { gql } = require("apollo-server-express");
 
 //TODO: create typeDefs
 const typeDefs = gql`
-  type Dummy {
+  type User {
     _id: ID
     name: String
+    email: String
+    password: String
+    friends: [User]
   }
 
-  type Query {
-    showDummy: [Dummy]
+  type Charity {
+    _id: ID
+    name: String
+    mission: String
+    location: String
+    website: String
+  }
+
+  type Drive {
+    _id: ID
+    organizer: User
+    charity: Charity
+    goal: Int
   }
 `;
 
