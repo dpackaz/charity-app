94e464e8165a65937f67f55aa206eda6545eeb12
Philip Huang <philiphuang2@gmail.com>
2022-12-13 10:16:39 -0800

    add causes to charity models

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -24,6 +24,7 @@ const typeDefs = gql`
     website_Url: String
     pledge_Url: String
     logo_Url: String
+    causes: [String]
   }
 
   input CharityInput {
@@ -34,6 +35,7 @@ const typeDefs = gql`
     website_Url: String
     pledge_Url: String
     logo_Url: String
+    causes:[String]
   }
 
   type Drive {
