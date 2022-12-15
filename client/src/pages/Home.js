66bd4d0fc28be7585a97ff46d4f3ec395f17e532
Philip Huang <philiphuang2@gmail.com>
2022-12-14 16:16:35 -0800

    added map to landing page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,18 +1,23 @@
 import React, { useState } from "react";
 import "../res/css/Home.css";
+import Map from '../components/Map'
+import { height } from "@amcharts/amcharts4/.internal/core/utils/Utils";
 
 const Home = () => {
   return (
-    <>
-      <div className="text-light bg-dark jumbotron text-center">
-        Test Jumbotron
-      </div>
-      <main>
-        <div className="flex-row justify-center">
-          <div className="col-12 col-md-8 mb-3"></div>
-        </div>
-      </main>
-    </>
+    // <>
+    //   <div className="text-light bg-dark jumbotron text-center">
+    //     Test Jumbotron
+    //   </div>
+    //   <main>
+    //     <div className="flex-row justify-center">
+    //       <div className="col-12 col-md-8 mb-3"></div>
+    //     </div>
+    //   </main>
+    // </>
+    <div className="bg-primary" style={{height:"100vh"}} >
+    <Map />
+    </div>
   );
 };
 
